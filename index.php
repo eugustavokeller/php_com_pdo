e6e921433396105e2b128d53ff7b722b0abd0a40
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2022-11-22 13:30:17 -0300

    Query with pdostatement

--- index.php
+++ index.php
@@ -8,39 +8,92 @@
         $conexao = new PDO($dsn, $usuario, $password);
 
         $query = '
-            create table if not exists tb_usuarios(
-                id int not null primary key auto_increment,
-                nome varchar(50) not null,
-                email varchar(100) not null,
-                senha varchar(32) not null
-            )
+            select * from tb_usuarios
         ';
 
-        $retorno = $conexao->exec($query);
+        $stmt = $conexao->query($query);
+        $lista = $stmt->fetchAll(); // fetchAll contem um array com todas as informações da query
 
-        echo $retorno;
+        echo '<pre>';
+            print_r($lista); // mostra todo o retorno do fetchAll
+        echo '</pre>';
 
-        /* INSERT
-        $query = '
-            insert into tb_usuarios(
-                nome,
-                email, 
-                senha
-            ) values(
-                "Gustavo",
-                "gustavo.lealtec@gmail.com",
-                "123456"
-            ) 
-        ';
-        */
-        // DELETE
-        $query = '
-            delete from tb_usuarios
-        ';        
-
-        $retorno = $conexao->exec($query);
-        echo $retorno;
+        echo $lista[0][2];
 
     } catch(PDOException $e) {
         echo 'Erro: '.$e->getCode(). ' Mensagem: '.$e->getMessage();
     }
+
+
+
+    // $query = '
+    //         create table if not exists tb_usuarios(
+    //             id int not null primary key auto_increment,
+    //             nome varchar(50) not null,
+    //             email varchar(100) not null,
+    //             senha varchar(32) not null
+    //         )
+    //     ';
+
+    //     $retorno = $conexao->exec($query);
+
+    //     echo $retorno;
+
+        // INSERT
+        // $query = '
+        //     insert into tb_usuarios(
+        //         nome,
+        //         email, 
+        //         senha
+        //     ) values(
+        //         "Gustavo",
+        //         "gustavo.lealtec@gmail.com",
+        //         "123456"
+        //     ) 
+        // ';
+
+        // DELETE
+        // $query = '
+        //     delete from tb_usuarios
+        // ';
+
+        // $query = '
+        //     insert into tb_usuarios(
+        //         nome,
+        //         email, 
+        //         senha
+        //     ) values(
+        //         "Gustavo Keller",
+        //         "gustavo.lealtec@gmail.com",
+        //         "123456"
+        //     ) 
+        // ';
+        // $retorno = $conexao->exec($query);
+
+        // $query = '
+        //     insert into tb_usuarios(
+        //         nome,
+        //         email, 
+        //         senha
+        //     ) values(
+        //         "Dyhenifer Keller",
+        //         "dyheni123@gmail.com",
+        //         "123456"
+        //     ) 
+        // ';
+        // $retorno = $conexao->exec($query);
+
+        // $query = '
+        //     insert into tb_usuarios(
+        //         nome,
+        //         email, 
+        //         senha
+        //     ) values(
+        //         "Junior Keller",
+        //         "juniorkeller@gmail.com",
+        //         "123456"
+        //     ) 
+        // ';
+
+        // $retorno = $conexao->exec($query);
+        // echo $retorno;
